7744c453e60b7858efad25d099ce65bb61ef4c3e
mahedi <mahedi_h@yahoo.com>
2020-09-13 12:52:18 +0600

    Updates

---
+++ precache-manifest.32ad615f74b02d5c9b56aa85591cd27e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "65b19333bc0c62f8dd3becb5e514b25f",
+    "url": "/contact_manager/index.html"
+  },
+  {
+    "revision": "2ee62cdc9b8802ae997d",
+    "url": "/contact_manager/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "75b7080bd0b2ee73cda2",
+    "url": "/contact_manager/static/css/main.36497136.chunk.css"
+  },
+  {
+    "revision": "2ee62cdc9b8802ae997d",
+    "url": "/contact_manager/static/js/2.f88592db.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/contact_manager/static/js/2.f88592db.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75b7080bd0b2ee73cda2",
+    "url": "/contact_manager/static/js/main.5cceb724.chunk.js"
+  },
+  {
+    "revision": "59a22c52cb5b0cb7b125",
+    "url": "/contact_manager/static/js/runtime-main.20b57675.js"
+  }
+]);
